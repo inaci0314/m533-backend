a129dde99763b4a060232e8c989baf7852b900f7
inaci0314 <47039618+inaci0314@users.noreply.github.com>
2020-10-15 16:07:34 +0200

    Update webshop.sql
    Added category and user tables
    Completed article table

--- src/data/webshop.sql
+++ src/data/webshop.sql
@@ -1,11 +1,28 @@
+/* Create webshop DB */
 CREATE DATABASE webshop DEFAULT CHARSET UTF8;
 USE webshop;
 
+/* Create category table */
+CREATE TABLE category (
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+)
+
+/* Create article table */
 CREATE TABLE article (
-    id INT UNSIGNED NOT NULL,
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    category_id INT UNSIGNED NOT NULL DEFAULT 0,
     name VARCHAR(50) NOT NULL,
     description VARCHAR(250),
     stock INT UNSIGNED NOT NULL,
     price FLOAT UNSIGNED NOT NULL,
-    
 );
+
+/* Create user table */
+CREATE TABLE user (
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+)
