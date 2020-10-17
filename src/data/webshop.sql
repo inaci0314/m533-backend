caf56d2e5b6055940f290584f05c135b31825be8
Carolina Pereira <inaciopereira.carolina@gmail.com>
2020-10-17 16:02:06 +0200

    fixed errors in sql script

--- src/data/webshop.sql
+++ src/data/webshop.sql
@@ -1,28 +1,29 @@
 /* Create webshop DB */
-CREATE DATABASE webshop DEFAULT CHARSET UTF8;
+CREATE DATABASE IF NOT EXISTS webshop DEFAULT CHARSET UTF8;
+
 USE webshop;
 
 /* Create category table */
-CREATE TABLE category (
+CREATE TABLE IF NOT EXISTS category (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL
-)
+);
 
 /* Create article table */
-CREATE TABLE article (
+CREATE TABLE IF NOT EXISTS article (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     category_id INT UNSIGNED NOT NULL DEFAULT 0,
     name VARCHAR(50) NOT NULL,
     description VARCHAR(250),
     stock INT UNSIGNED NOT NULL,
-    price FLOAT UNSIGNED NOT NULL,
+    price FLOAT UNSIGNED NOT NULL
 );
 
 /* Create user table */
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
     first_name VARCHAR(50),
     last_name VARCHAR(50)
-)
+);
