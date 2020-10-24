23a163e44568101eead2fc4ccb7eeff661fc490f
Carolina Pereira <inaciopereira.carolina@gmail.com>
2020-10-24 14:30:16 +0200

    changed index to use the config file, added routes

--- src/public/index.php
+++ src/public/index.php
@@ -1,16 +1,17 @@
 <?php
 
 /* Config */
-$config['displayErrorDetails'] = true;
-$config['addContentLengthHeader'] = false;
 
-$config['db']['host']   = 'localhost';
-$config['db']['user']   = 'user';
-$config['db']['pass']   = 'password';
-$config['db']['dbname'] = 'exampleapp';
+$configfile = $_SERVER['DOCUMENT_ROOT'] . '/src/config/config.ini';
+$test = parse_ini_file($configfile);
+
+$config = $test;
+
 /* Config END */
 
 require '../../vendor/autoload.php';
+require '../classes/data/sql/SqlConnectionManager.php';
+require '../classes/data/sql/SqlCategoryRepository.php';
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
@@ -19,7 +20,9 @@ use \Slim\Http\Response as ErrorResponse;
 $app = new \Slim\App(['settings' => $config]);
 
 /* Dependencies */
+
 $container = $app->getContainer();
+
 // logger
 $container['logger'] = function ($c) {
     $logger = new \Monolog\Logger('my_logger');
@@ -29,23 +32,68 @@ $container['logger'] = function ($c) {
 };
 
 // db
-$container['db'] = function ($c) {
-    $db = $c['settings']['db'];
-    $pdo = new PDO(
-        'mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
-        $db['user'],
-        $db['pass']
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    return $pdo;
+$db = $config['db'];
+$container['connectionMgr'] = function ($c) {
+    $db = $c["settings"]["db"];
+    return new SqlConnectionManager($db['host'], $db['dbname'], $db['user'], $db['pass']);
 };
+
 /* Dependencies END */
 
 /* Routes */
-// home
+
+// test
 $app->get('/', function (Request $request, Response $response) {
     return $response->getBody()->write("hello");
+});
+
+// get all categories
+$app->get('/categories', function (Request $request, Response $response) {
+    $connectionMgr = $this->connectionMgr;
+    $categoryRepository = new SqlCategoryRepository($connectionMgr);
+
+    $categories = $categoryRepository->getAllCategories();
+
+    $res = array();
+    foreach ($categories as $category) {
+        $res[] = array(
+            "id" => $category->getId(),
+            "name" => $category->getName()
+        );
+    }
+
+    return $response->getBody()->write(json_encode($res));
+});
+
+// get category by id
+$app->get('/categories/{id}', function (Request $request, Response $response, $args) {
+    $id = $args["id"];
+
+    $connectionMgr = $this->connectionMgr;
+    $categoryRepository = new SqlCategoryRepository($connectionMgr);
+
+    $category = $categoryRepository->getCategoryById($id);
+
+    $res = array();
+    // If no category was found, return error code 404
+    if (!isset($category)) {
+        $response = $response->withStatus(404);
+    } else {
+        $res = array(
+            "id" => $category->getId(),
+            "name" => $category->getName()
+        );
+
+        $response->getBody()->write(json_encode($res));
+    }
+
+    return $response;
+});
+
+// articles by category
+$app->get('/articles/{categoryId}', function (Request $request, Response $response, $args) {
+    $categoryId = $args["categoryId"];
+    return $response->getBody()->write("categoryId: $categoryId");
 });
 
 // search articles
@@ -58,9 +106,7 @@ $app->get('/search', function (Request $request, Response $response) {
     return $response->getBody()->write("Search results for '$query'");
 });
 
-// articles by category
-$app->get('/articles/{categoryId}', function () {
-});
+
 
 // article details
 $app->get('/article/{id}', function (Request $request, Response $response, $args) {
@@ -75,7 +121,6 @@ $app->get('/cart', function () {
 // checkout
 $app->get('/checkout', function () {
 });
-
 
 /* Routes END */
 
