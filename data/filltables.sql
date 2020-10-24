ad2e468009e0ec45e9edbd1bc4a575cdca068399
Carolina Pereira <inaciopereira.carolina@gmail.com>
2020-10-24 14:22:20 +0200

    Added filltables.sql

---
+++ data/filltables.sql
@@ -0,0 +1,13 @@
+USE webshop;
+-- fill category
+INSERT INTO categories(name) VALUES('Category1');
+INSERT INTO categories(name) VALUES('Category2');
+INSERT INTO categories(name) VALUES('Category3');
+INSERT INTO categories(name) VALUES('Category4');
+INSERT INTO categories(name) VALUES('Category5');
+-- fill article
+INSERT INTO articles(name, description, stock, price) VALUES('Article1', '', 25, 125.50);
+INSERT INTO articles(name, description, stock, price) VALUES('Article2', '', 53, 3.50);
+INSERT INTO articles(name, description, stock, price) VALUES('Article3', '', 80, 49.95);
+INSERT INTO articles(name, description, stock, price) VALUES('Article4', '', 37, 250);
+INSERT INTO articles(name, description, stock, price) VALUES('Article5', '', 60, 14.95);
